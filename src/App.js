4cbede34b6f74be19e4007cf1fb33ca8c8a44577
tanmaytirpankar <tirpankartanmay@gmail.com>
2021-08-30 18:29:45 -0600

    Started react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default () => {
+
+    return (
+        <div>
+            <i className={`title-icon bug massive icon`} />
+            <h1 className="title">Seesaw</h1>
+        </div>
+    )
 }
 
-export default App;
